afb4b673b7b289524abd00b9a8118e9700624305
Mr.Meng <2746553317@qq.com>
2021-11-03 09:13:40 +0800

    Initial commit

---
+++ app/src/androidTest/java/me/blankm/progressmanager/demo/ExampleInstrumentedTest.java
@@ -0,0 +1,10 @@
+package me.blankm.progressmanager.demo;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a.java href="http://d.android.com/tools/testing">Testing documentation</a.java>
+ */
+public class ExampleInstrumentedTest {
+
+}
